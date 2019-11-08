ba769743bfd647c3590f419a46069e89177aee2f
217861 <217861@edu.p.lodz.pl>
2019-11-08 15:52:38 +0100

    Add SQL Database connection, tables and basic tests

---
+++ Farm/FarmTes/DatabaseManagerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Farm;
+using System.Data;
+using System;
+
+namespace FarmTest
+{
+    [TestClass]
+    public class DatabaseManagerTest1
+    {
+        [TestMethod]
+        public void TestingAlreadyInsertedRow()
+        {
+            DatabaseManager manager = new DatabaseManager();
+            DataTable outcome = manager.RunQuery("select * from Animals where Id = 1");
+            Assert.AreEqual("Cow", outcome.Rows[0]["Species"]);
+            Assert.AreEqual("M", outcome.Rows[0]["Sex"]);
+        }
+
+        [TestMethod]
+        public void InsertingRowAndTesting()
+        {
+            DatabaseManager manager = new DatabaseManager();
+            manager.ExecuteInstruction("insert into Animals(Species, Sex, Birth_date) values ('Pig', 'F', '08-11-19 10:34:00')");
+            DataTable outcome = manager.RunQuery("select * from Animals where Id = 2");
+            Assert.AreEqual("Pig", outcome.Rows[0]["Species"]); 
+            DateTime date = new DateTime(2019, 8, 11, 10, 34, 0);
+            Assert.AreEqual(date, outcome.Rows[0]["Birth_date"]);
+            manager.ExecuteInstruction("delete from Animals where Id=2");
+        }
+    }
+}
---
+++ Farm/FarmTes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FarmTes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FarmTes")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1509b51d-ddbf-4f16-a11b-f39bd86e952b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
